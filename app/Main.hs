692774cc44bb5a1a7972772cfdb578e3f24aeb67
Creepsy <Robin.gd@outlook.de>
2023-02-25 11:59:36 +0100

    generalization of types

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,13 @@ import Data.List (singleton)
 import qualified Data.Matrix as Mat
 import qualified Data.Vector as Vec
 
-type Term = [Rational]
-type Inequation = (Term, Rational) -- we assume <= here, as this is the type of inequation commonly used for the simplex algorithm
+type Term a = [a]
+type Inequation a = (Term a, a) -- we assume <= here, as this is the type of inequation commonly used for the simplex algorithm
 
 main :: IO ()
 main = print (simplex [([1, 2], 1000), ([3, 4], 3500)] [5, 20])
 
--- use matrix & vector
-simplex :: [Inequation] -> Term -> [Rational]
+simplex :: (Fractional a, Ord a, Eq a) => [Inequation a] -> Term a -> [a]
 simplex inequations toMaximize = extractSolution finalTable where
     inequationCount = length inequations
     isFinished table =Vec.all (>=0) . Mat.getRow (Mat.nrows table) $ table
@@ -19,7 +18,7 @@ simplex inequations toMaximize = extractSolution finalTable where
     initialTable = buildSimplexTable inequations toMaximize
     finalTable = head . dropWhile (not . isFinished) . iterate simplexStep $ initialTable
 
-simplexStep :: Mat.Matrix Rational -> Mat.Matrix Rational
+simplexStep :: (Fractional a, Ord a) => Mat.Matrix a -> Mat.Matrix a
 simplexStep simplexTable = simplexTable'' where
     pivotPos@(pivotRow, pivotColumn) = pivotElement simplexTable
     pivot = simplexTable Mat.! pivotPos
@@ -30,7 +29,7 @@ simplexStep simplexTable = simplexTable'' where
     zeroPivotColumn row table = Mat.mapRow (\col e -> e - multiplier * table Mat.! (pivotRow, col)) row table where
         multiplier = table Mat.! (row, pivotColumn)
 
-extractSolution :: Mat.Matrix Rational -> [Rational]
+extractSolution :: (Fractional a, Eq a) => Mat.Matrix a -> [a]
 extractSolution simplexTable = [if partOfSolution var then getVariableValue var else 0 | var <- [1..variableCount]] where
     variableCount = Mat.ncols simplexTable - Mat.nrows simplexTable
 
@@ -40,7 +39,7 @@ extractSolution simplexTable = [if partOfSolution var then getVariableValue var 
     getVariableValue column = simplexTable Mat.! (row, Mat.ncols simplexTable) where
         (Just row) = (+1) <$> Vec.findIndex (==1) (Mat.getCol column simplexTable)
 
-pivotElement :: Mat.Matrix Rational -> (Int, Int)
+pivotElement :: (Fractional a, Ord a) => Mat.Matrix a -> (Int, Int)
 pivotElement simplexTable = (pivotRow, pivotColumn) where
     toMaximizeRow = Vec.init . Mat.getRow (Mat.nrows simplexTable) $ simplexTable
     bounds = Vec.init . Mat.getCol (Mat.ncols simplexTable) $ simplexTable
@@ -48,7 +47,7 @@ pivotElement simplexTable = (pivotRow, pivotColumn) where
     pivotColumn = Vec.minIndex toMaximizeRow + 1
     pivotRow = Vec.minIndex (Vec.zipWith (/) bounds (Mat.getCol pivotColumn simplexTable)) + 1
 
-buildSimplexTable :: [Inequation] -> Term -> Mat.Matrix Rational
+buildSimplexTable :: Fractional a => [Inequation a] -> Term a -> Mat.Matrix a
 buildSimplexTable inequations toMaximize = inequationsTable Mat.<|> helperVariables Mat.<|> bounds where
     variableCount = maximum . map (length . fst) $ inequations
     inequations' = map (fill variableCount 0 . fst) inequations
